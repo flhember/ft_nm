3b6e79ed1ab0e88e2d2f04f5bd8cf7865a4d00db
flhember <florian.hembertjaj@gmail.com>
2021-04-15 19:00:28 +0200

    oui

---
+++ includes/ft_nm.h
@@ -0,0 +1,8 @@
+#ifndef FT_NM_H
+# define FT_NM_H
+
+# include "../../libft/includes/libft.h"
+
+void	ft_nm(int ac, char **av);
+
+#endif
---
+++ srcs/ft_nm.c
@@ -0,0 +1,6 @@
+#include "ft_nm.h"
+
+void	ft_nm(int ac, char *av)
+{
+	ft_printf("%d\n", ac);
+}
